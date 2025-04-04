747a876266990133672ff8e4c404fb273e00b028
Will <william.keiser67@gmail.com>
2025-04-04 21:36:20 +0200

    COnfigParser symbiose

--- includes/ConfigParser.hpp
+++ includes/ConfigParser.hpp
@@ -2,6 +2,7 @@
 #define CONFIGPARSER_HPP
 
 #include "ServerConfig.hpp"
+#include "ConfigLexer.hpp" 
 #include <vector>
 #include <string>
 #include <fstream>
@@ -15,18 +16,20 @@ class ConfigParser
 	std::vector<ServerConfig> m_servers;
 
 	void parse();                    // Parses entire file
-	void parseServer(std::istream&); // Parses a single `server` block
+	//void parseServer(std::istream&); // Parses a single `server` block
 
-	static std::string trim(const std::string& line);
-	static std::vector<std::string> split(const std::string& line);
+	//static std::string trim(const std::string& line);
+	//static std::vector<std::string> split(const std::string& line);
+	// Recursively parses a server block from tokens.
+	ServerConfig parseServerBlock(const std::vector<Token>& tokens, size_t &current);
 
 	public:
 	ConsfigParser();
+	ConfigParser(const std::string &path);
 	ConfigParser(const ConfigParser &src);
 	ConfigParser &operator=(const ConfigParser &src);
 	~ConfigParser();
 
-	ConfigParser(const std::string &path);
 	const std::vector<ServerConfig> &getServers() const;
 };
 
