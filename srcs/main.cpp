014c2360e8f9ce28481a1caa0cd86e9d64e4ec91
Max Govindaraju <mgovinda@k0r1p9.42mulhouse.fr>
2025-04-04 14:54:24 +0200

    ConfigParser

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,11 +6,12 @@
 /*   By: mgovinda <mgovinda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 16:04:39 by mgovinda          #+#    #+#             */
-/*   Updated: 2025/03/20 16:06:04 by mgovinda         ###   ########.fr       */
+/*   Updated: 2025/04/04 14:53:12 by mgovinda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include "ConfigPaerser.hpp"
 
 int main(int argc, char **argv)
 {
