41d6b148ae746e9f762c124564f5d878188eac83
Max Govindaraju <mgovinda@k0r1p9.42mulhouse.fr>
2025-04-04 15:03:59 +0200

    main

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mgovinda <mgovinda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 16:04:39 by mgovinda          #+#    #+#             */
-/*   Updated: 2025/04/04 14:53:12 by mgovinda         ###   ########.fr       */
+/*   Updated: 2025/04/04 15:02:37 by mgovinda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,23 @@
 
 int main(int argc, char **argv)
 {
-	(void) argc;
-	(void) argv;
-
-	std::cout << "Henlo !" << std::endl;
+	std::string cfg = "default.conf"
+	if (argc == 2)
+		cfg = argv[1];
+	else if ( argv > 2)
+	{
+		std::cerr << "Usage: ./webserv [configuration file]" << std::endl;
+		return (1);
+	}
+	try	
+	{
+		ConfigParser parser(cfg);
+		std::vector<ServerConfig> servers = parser.getServers();
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "Error parsing config file: " << e.what() << std::endl;
+		return (1);
+	}
+	return (0);
 }
