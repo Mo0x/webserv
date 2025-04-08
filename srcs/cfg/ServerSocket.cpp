0aaf41c941f1325f459889f2e6e197de4e3101b0
Max Govindaraju <mgovinda@k2r2p10.42mulhouse.fr>
2025-04-08 17:43:34 +0200

    start of socker server

--- srcs/cfg/ServerSocket.cpp
+++ srcs/cfg/ServerSocket.cpp
@@ -6,7 +6,7 @@
 /*   By: mgovinda <mgovinda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 16:45:28 by mgovinda          #+#    #+#             */
-/*   Updated: 2025/04/08 17:02:09 by mgovinda         ###   ########.fr       */
+/*   Updated: 2025/04/08 17:43:02 by mgovinda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,4 +45,69 @@ ServerSocket& ServerSocket::operator=(const ServerSocket& other)
 		m_host = other.m_host;
 	}
 	return *this;
+}
+
+int ServerSocket::getFd() const
+{
+	return m_fd;
+}
+
+unsigned short ServerSocket::getPort() const
+{
+	return m_port;
+}
+
+const std::string& ServerSocket::getHost() const
+{
+	return m_host;
+}
+
+
+void ServerSocket::setup()
+{
+	m_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (m_fd < 0)
+		throw std::runtime_error("socket() failed: " + std::string(strerror(errno)));
+
+	int opt = 1;
+	if (setsockopt(m_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0)
+		throw std::runtime_error("setsockopt() failed: " + std::string(strerror(errno)));
+
+	setNonBlocking();
+	bindSocket();
+	listenSocket();
+}
+
+void ServerSocket::setNonBlocking()
+{
+	int flags = fcntl(m_fd, F_GETFL, 0);
+	if (flags < 0)
+		throw std::runtime_error("fcntl(F_GETFL) failed: " + std::string(strerror(errno)));
+
+	if (fcntl(m_fd, F_SETFL, flags | O_NONBLOCK) < 0)
+		throw std::runtime_error("fcntl(F_SETFL) failed: " + std::string(strerror(errno)));
+}
+
+void ServerSocket::bindSocket()
+{
+	struct sockaddr_in addr;
+	std::memset(&addr, 0, sizeof(addr));
+
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(m_port);
+	addr.sin_addr.s_addr = inet_addr(m_host.c_str());
+
+	if (bind(m_fd, (struct sockaddr*)&addr, sizeof(addr)) < 0)
+	{
+		std::ostringstream oss;
+		oss << "bind() failed on " << m_host << ":" << m_port << " - " << strerror(errno);
+		throw std::runtime_error(oss.str());
+	}
+}
+
+
+void ServerSocket::listenSocket()
+{
+	if (listen(m_fd, 128) < 0)
+		throw std::runtime_error("listen() failed: " + std::string(strerror(errno)));
 }
