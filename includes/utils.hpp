d889a2b1a4105ef0be9685c79c42971b3ea406e4
Mo0x <max.govindaraju@gmail.com>
2025-06-09 19:16:30 +0200

    FEATURE: added findMatchingLocation

--- includes/utils.hpp
+++ includes/utils.hpp
@@ -2,7 +2,9 @@
 #define UTILS_HPP
 
 #include <iostream>
+#include "Config.hpp"
 
 std::string to_string(size_t val);
+const RouteConfig *findMatchingLocation(const ServerConfig &server, const std::string &path);
 
 #endif
--- srcs/utils/utils.cpp
+++ srcs/utils/utils.cpp
@@ -7,3 +7,19 @@ std::string to_string(size_t val)
 	oss << val;
 	return oss.str();
 }
+
+/* Give this longest prefix IE: location /upload is taken vs location /*/
+
+const RouteConfig* findMatchingLocation(const ServerConfig& server, const std::string& path) {
+	const RouteConfig* bestMatch = NULL;
+	size_t bestLength = 0;
+
+	for (size_t i = 0; i < server.routes.size(); ++i) {
+		const RouteConfig& route = server.routes[i];
+		if (path.find(route.path) == 0 && route.path.length() > bestLength) {
+			bestMatch = &route;
+			bestLength = route.path.length();
+		}
+	}
+	return bestMatch;
+}
