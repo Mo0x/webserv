891b453ed53da9d20a6138abe11815a6bb231d9e
Mo0x <max.govindaraju@gmail.com>
2025-10-27 19:05:06 +0100

    cleaning socket manager and adding clientState struct

--- includes/SocketManager.hpp
+++ includes/SocketManager.hpp
@@ -6,7 +6,7 @@
 /*   By: mgovinda <mgovinda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/13 18:37:22 by mgovinda          #+#    #+#             */
-/*   Updated: 2025/10/16 21:10:38 by mgovinda         ###   ########.fr       */
+/*   Updated: 2025/10/27 19:04:40 by mgovinda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,40 +17,64 @@
 #include "utils.hpp"
 #include "Config.hpp"
 #include "request_parser.hpp"
+#include "Chunked.hpp"
 #include <vector>
 #include <poll.h>
 #include <map>
 #include <string>
-#include <set> // NEW
+#include <set>
+
+struct ClientState
+{
+	enum Phase
+	{
+		READING_HEADERS,
+		READING_BODY,
+		READTY_TO_DISPATCH,
+		SENDING_RESPONSE,
+		CLOSED
+	};
+
+	Phase			phase;
+	std::string		recvBuffer;
+	Request			req;
+	bool			isChunked;
+	size_t			contentLenght;
+	size_t			maxBodyAllowed;
+
+	std::string		bodyBuffer;
+	ChunkedDecoder	chunkDec;
+};
 
 class SocketManager
 {
 	private:
-	std::vector<ServerSocket*> m_servers;
-	std::vector<struct pollfd> m_pollfds;
-	std::set<int>                  m_serverFds;     // to distinguish server vs client
-    std::map<int, std::string>     m_clientBuffers; // client fd → partial data
+	std::vector<ServerSocket*>	m_servers;
+	std::vector<struct pollfd>	m_pollfds;
+	std::set<int>				m_serverFds;     // to distinguish server vs client
+    std::map<int, std::string> 	m_clientBuffers; // client fd → partial data
 	std::map<int, std::string>	m_clientWriteBuffers;
-	std::vector<ServerConfig> m_serversConfig;
-	Config	m_config;
+	std::map<int, ClientState>	m_clients;
+	std::vector<ServerConfig>	m_serversConfig;
+	Config						m_config;
 
-	std::map<int, size_t> m_clientToServerIndex;
+	std::map<int, size_t>		m_clientToServerIndex;
 
 	//for max body size:
-	std::map<int, bool>		m_headersDone;
-	std::map<int, size_t>	m_expectedContentLen;
-	std::map<int, size_t>	m_allowedMaxBody;
+	std::map<int, bool>			m_headersDone;
+	std::map<int, size_t>		m_expectedContentLen;
+	std::map<int, size_t>		m_allowedMaxBody;
 
 	SocketManager &operator=(const SocketManager &src);
 	SocketManager(const SocketManager &src);
 	std::map<int, bool> m_isChunked;
 
 	//for keep-alive connection
-        std::map<int, bool> m_closeAfterWrite;
+	std::map<int, bool> m_closeAfterWrite;
 
-        public:
-        SocketManager(const Config &config);
-        SocketManager();
+	public:
+	SocketManager(const Config &config);
+	SocketManager();
 	~SocketManager();
 
 	void addServer(const std::string& host, unsigned short port);
@@ -71,30 +95,30 @@ class SocketManager
 
 	void setServers(const std::vector<ServerConfig> & servers);
 	
-        void finalizeAndQueue(int fd, const Request &req, Response &res, bool body_expected, bool body_fully_consumed);
-        void finalizeAndQueue(int fd, Response &res);
-        //for keep-alive
-        bool shouldCloseAfterThisResponse(int status_code, bool headers_complete, bool body_expected, bool body_fully_consumed, bool client_close) const;
+	void finalizeAndQueue(int fd, const Request &req, Response &res, bool body_expected, bool body_fully_consumed);
+	void finalizeAndQueue(int fd, Response &res);
+	//for keep-alive
+	bool shouldCloseAfterThisResponse(int status_code, bool headers_complete, bool body_expected, bool body_fully_consumed, bool client_close) const;
 
-    private:
-        bool readIntoClientBuffer(int fd);
-        bool locateHeaders(int fd, size_t &hdrEnd);
-        bool enforceHeaderLimits(int fd, size_t hdrEnd);
-        bool parseAndValidateRequest(int fd, size_t hdrEnd, Request &req,
-                                     const ServerConfig* &server,
-                                     std::string &methodUpper,
-                                     size_t &contentLength,
-                                     bool &hasTE);
-        bool processFirstTimeHeaders(int fd, const Request &req,
-                                     const ServerConfig &server,
-                                     const std::string &methodUpper,
-                                     bool hasTE,
-                                     size_t contentLength);
-        bool ensureBodyReady(int fd, size_t hdrEnd, size_t &requestEnd);
-        void dispatchRequest(int fd, const Request &req,
-                             const ServerConfig &server,
-                             const std::string &methodUpper);
-        void resetRequestState(int fd);
+	private:
+	bool readIntoClientBuffer(int fd);
+	bool locateHeaders(int fd, size_t &hdrEnd);
+	bool enforceHeaderLimits(int fd, size_t hdrEnd);
+	bool parseAndValidateRequest(int fd, size_t hdrEnd, Request &req,
+									const ServerConfig* &server,
+									std::string &methodUpper,
+									size_t &contentLength,
+									bool &hasTE);
+	bool processFirstTimeHeaders(int fd, const Request &req,
+									const ServerConfig &server,
+									const std::string &methodUpper,
+									bool hasTE,
+									size_t contentLength);
+	bool ensureBodyReady(int fd, size_t hdrEnd, size_t &requestEnd);
+	void dispatchRequest(int fd, const Request &req,
+							const ServerConfig &server,
+							const std::string &methodUpper);
+	void resetRequestState(int fd);
 };
 
 #endif
