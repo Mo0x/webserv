cceb3c59b64719692117f197be2804c646dbff59
Mo0x <max.govindaraju@gmail.com>
2025-11-06 17:04:57 +0100

    added: helpers for handlePostUploadAndCgi

--- srcs/server/SocketManagerPost.cpp
+++ srcs/server/SocketManagerPost.cpp
@@ -24,3 +24,37 @@ static std::string joinPath(const std::string &a, const std::string &b)
 		return a + "/" + b;
 	}
 }
+
+//create a name, and avoid collision (using timestamp from epoch)
+static std::string makeUploadFileName(const std::string &hint)
+{
+	static unsigned long s_counter = 0;
+	++s_counter;
+
+	// avoid slash from hint
+
+	std::string base = hint;
+	size_t pos = base.rfind('/');
+	if (pos != std::string::npos)
+		base = base.substr(pos +1);
+	if (base.empty())
+		base = "upload";
+	
+	//strip dangereous chars (basically we only take AZ_az-09.
+	for (size_t i = 0; i < base.size(); ++i)
+	{
+		char &c = base[i];
+		if (!(c >= 'a' && c <= 'z') ||
+			(c >= 'A' && c <= 'Z') ||
+			(c >= '0' && c <= '9') ||
+			c == '_' || c == '-' || c == '.'
+		)
+		{
+			c = '_';
+		}
+	}
+
+	std::ostringstream oss;
+	oss << base << (unsigned long)time(NULL) << "_" << s_counter;
+	return oss.str();
+}
